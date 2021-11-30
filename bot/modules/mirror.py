c911574d05d98314e6abb6fdc9cbad065030b9d2
Seng Kyaut <sengkyaut@hotmail.com>
2021-11-30 23:55:49 +0630

    Fix typo

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -254,7 +254,7 @@ class MirrorListener(listeners.MirrorListeners):
                 update_all_messages()
             return
         elif self.isMytel:
-            real_files = files[item].split("\n\n")
+            real_files = files.split("\n\n")
             count = len(real_file)
             chat_id = str(self.message.chat.id)[4:]
             msg = f"<b>Name:</b> <a href='https://t.me/c/{chat_id}/{self.uid}'>{link} {get_readable_file_size(size)}</a>\n"
